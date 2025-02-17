05e6bdec16e3b13b9047d614e17bec6c57f4981a
marques-svnt <marques.svnt.002@gmail.com>
2025-02-16 23:25:00 -0300

    Criação de lógica para exibir na matriz de leds e controle através dos botões para escolher os diferentes módulos de sensor simulado, também aparece no display, embora mal configurado

--- main.c
+++ main.c
@@ -1,16 +1,23 @@
+// Bibliotecas padrão em C
 #include <stdio.h>
 #include <stdlib.h>
+
+// Bibliotecas de hardware do Raspberry Pi Pico
 #include "pico/stdlib.h"
-#include "defines.h"
 #include "hardware/adc.h"
-#include "hardware/pwm.h"
-#include "hardware/pio.h"
 #include "hardware/clocks.h"
+#include "hardware/pio.h"
+#include "hardware/pwm.h"
 #include "hardware/timer.h"
+
+// Headers do projeto
+#include "defines.h"
 #include "display.h"
 #include "init.h"
-#include "interrupt.h"
+#include "menu.h"
 #include "pwm.h"
+#include "pio.h"
+
 
 int main()
 {
@@ -18,6 +25,15 @@ int main()
     // Função que chama todas as inicializações do projeto
     init();
 
+    set_one_led(1, 0, 0, 20);
+
+    display("Sensor", 45, 20);
+    display("Temperatura", 25, 35);
+
+    // Configuração da interrupção com callback
+    gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    
     // Loop principal
     while (true)
     {
---
+++ src/menu.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "defines.h"
+#include "display.h"
+#include "pio.h"
+
+// Variável global para armazenar a cor (Entre 0 e 255 para intensidade)
+uint8_t led_r = 0;  // Intensidade do vermelho
+uint8_t led_g = 0;  // Intensidade do verde
+uint8_t led_b = 20; // Intensidade do azul
+
+// Variáveis globais
+static volatile uint a = 1;
+static volatile uint32_t last_time_A = 0; // Armazena o tempo do último evento (em microssegundos)
+static volatile uint32_t last_time_B = 0;
+static volatile int choose = 1;
+
+void display_menu()
+{
+    switch (choose)
+    {
+    case 1:
+        limpar();
+        display("Sensor", 45, 20);
+        display("Temperatura", 25, 35);
+        break;
+    case 2:
+        limpar();
+        display("Sensor ", 45, 20);
+        display("Movimento", 25, 35);
+        break;
+    case 3:
+        limpar();
+        display("Sensor ", 45, 20);
+        display("Cardiaco", 25, 35);
+        break;
+    case 4:
+        limpar();
+        display("Configurar", 30, 20);
+        break;
+    }
+}
+
+// Função responsável pelo debounce
+bool debounce(volatile uint32_t *last_time, uint32_t debounce_time)
+{
+    uint32_t current_time = to_us_since_boot(get_absolute_time());
+    if (current_time - *last_time > debounce_time)
+    {
+        *last_time = current_time;
+        return true;
+    }
+    return false;
+}
+
+// Função de interrupção com debouncing
+void gpio_irq_handler(uint gpio, uint32_t events)
+{
+    uint32_t current_time = to_us_since_boot(get_absolute_time());
+
+    // Incrementação do número da matriz de leds
+    if (gpio == BUTTON_A && debounce(&last_time_A, 200000))
+    {
+        last_time_A = current_time;
+
+        if (choose == 1)
+        {
+            choose = 4;
+        }
+        else
+        {
+            choose--;
+        }
+        set_one_led(choose, led_r, led_g, led_b);
+        display_menu(choose);
+    }
+    // Decrementação do número da matriz de leds
+    else if (gpio == BUTTON_B && debounce(&last_time_B, 200000))
+    {
+        last_time_B = current_time;
+
+        if (choose == 4)
+        {
+            choose = 1;
+        }
+        else
+        {
+            choose++;
+        }
+        set_one_led(choose, led_r, led_g, led_b);
+        display_menu(choose);
+    }
+}

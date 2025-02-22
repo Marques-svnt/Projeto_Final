62a1d3657f9fc3b4be230facd339bb1e6917f85f
marques-svnt <marques.svnt.002@gmail.com>
2025-02-22 00:27:50 -0300

    Adição de setas e sinais de alarme na fonte e mudança na lógica dos botões

--- src/init.c
+++ src/init.c
@@ -53,15 +53,15 @@ void menu_init(){
 
     // Exibição inicial
     set_one_led(1, 0, 0, 20);
-    display("Sensor", 45, 20);
-    display("Temperatura", 25, 35);
+    display("Sensor", 40, 15);
+    display("Temperatura", 20, 25);
+    display(" <     B     >",4,45);
 
     choose = 1;
 
     // Configuração da interrupção com callback
     gpio_set_irq_enabled_with_callback(BUTTON_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
     gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
-    gpio_set_irq_enabled_with_callback(JOYSTICK_PB, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 }
 
 // Faz todas as inicializações do projeto
